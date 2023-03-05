20ea1395ff1d2a134caf525d0febde9c463dee8a
raczko92 <122747141+raczko92@users.noreply.github.com>
2023-03-05 19:55:45 +0100

    first commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const Start = document.querySelector("button[data-start]");
+const Stop = document.querySelector("button[data-stop]");
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function changer() {
+    Interval = setInterval(() => {
+    document.body.style.background = getRandomHexColor()
+    }, 1000)
+} 
+
+function stoper() {
+    clearInterval(Interval)
+}
+
+Start.addEventListener("click", changer );
+Stop.addEventListener("click", stoper )
